b01d11bd71be24d287058926b50541753819b822
root <timo.vink@andreaswerk.de>
2023-01-18 12:31:51 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,371 @@
+const allCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+const charsAndNumbers = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+];
+
+const onlyChars = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+const charsAndSymbols = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let passwordLength = 15;
+let chosenCharacterList = allCharacters;
+
+let useNumbers = true;
+let useSymbols = true;
+
+function increase() {
+  if (passwordLength < 18) {
+    passwordLength++;
+    document.getElementById("password-length").textContent = passwordLength;
+  }
+}
+
+function decrease() {
+  if (passwordLength > 6) {
+    passwordLength--;
+    document.getElementById("password-length").textContent = passwordLength;
+  }
+}
+
+const getPasswords = () => {
+  if (useNumbers && useSymbols) chosenCharacterList = allCharacters;
+  else if (!useNumbers && !useSymbols) chosenCharacterList = onlyChars;
+  else if (useNumbers && !useSymbols) chosenCharacterList = charsAndNumbers;
+  else if (!useNumbers && useSymbols) chosenCharacterList = charsAndSymbols;
+
+  document.getElementById("first-output").textContent = generatePassword(
+    passwordLength,
+    chosenCharacterList
+  );
+  document.getElementById("second-output").textContent = generatePassword(
+    passwordLength,
+    chosenCharacterList
+  );
+};
+
+const generatePassword = (length, characters) => {
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    password += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return password;
+};
+
+function toggleNumbers() {
+  if (useNumbers) {
+    document.getElementById("number-checkbox").textContent = "☐ use Numbers";
+  } else
+    document.getElementById("number-checkbox").textContent = "☒ use Numbers";
+
+  useNumbers = !useNumbers;
+}
+
+function toggleSymbols() {
+  if (useSymbols) {
+    document.getElementById("symbol-checkbox").textContent = "☐ use Symbols";
+  } else
+    document.getElementById("symbol-checkbox").textContent = "☒ use Symbols";
+
+  useSymbols = !useSymbols;
+}
+
+function saveToClipboard(i) {
+  if (i === 1)
+    navigator.clipboard.writeText(
+      document.getElementById("first-output").textContent
+    );
+  else if (i === 2)
+    navigator.clipboard.writeText(
+      document.getElementById("second-output").textContent
+    );
+}
